f2a51279d03ed9a76f72b1b21c366abb192f057b
Hazem <junbsher@gmail.com>
2022-12-04 19:50:55 -0600

    java subString assugnment

--- src/com/perscholas/scannerClassAssignment/ScannerClass.java
+++ src/com/perscholas/scannerClassAssignment/ScannerClass.java
@@ -37,5 +37,39 @@ public class ScannerClass {
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
     }
+	
+	// Java Substring
+	
+	/*
+	 Given a string, , and two indices,  and , print a substring consisting of all 
+	 characters in the inclusive range from  to . 
+	 You'll find the String class' substring method helpful in completing this challenge.
 
+	Input Format
+	
+	The first line contains a single string denoting .
+	The second line contains two space-separated integers denoting the respective values of  and .
+	
+	Constraints
+	
+	String  consists of English alphabetic letters (i.e., ) only.
+	Output Format
+	
+	Print the substring in the inclusive range from  to .
+	
+	Sample Input
+	
+	Helloworld
+	3 7
+	Sample Output
+	
+	lowo
+	 */
+
+	Scanner in = new Scanner(System.in);
+    String S = in.next();
+    int start = in.nextInt();
+    int end = in.nextInt();
+	
+	
 }
